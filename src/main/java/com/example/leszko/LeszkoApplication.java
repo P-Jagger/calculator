e398a4efab23316a571a0cb94a7502414e29665b
Sai Punk <saipunk@Sais-MacBook-Air.local>
2023-05-31 14:11:06 +0630

    first

---
+++ src/main/java/com/example/leszko/LeszkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.leszko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeszkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeszkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leszko/LeszkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leszko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeszkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
